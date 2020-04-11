6fcd0317479d07b250460ceec6e0b3bf7507815a
Faded-Hero <58606807+Faded-Hero@users.noreply.github.com>
2020-04-11 23:44:29 +0400

    Add files via upload

--- 2.py
+++ 2.py
@@ -5,8 +5,11 @@ import requests
 
 WIDTH = 450
 HEIGHT = 450
+lon = '53.200425'
+lat = '56.867347'
+size = 10
 
-def map_request(lo, la, Width, Height):
+def map_request(lo, la, Width, Height, size):
     api_server = "http://static-maps.yandex.ru/1.x/"
 
     lon = lo
@@ -24,17 +27,22 @@ def map_request(lo, la, Width, Height):
     response = requests.get(api_server, params=params)
     return response
 
-response = requests.get(map_request)
-map_file = "map.png"
-with open(map_file, "wb") as file:
-    file.write(response.content)
 
 pygame.init()
-screen = pygame.display.set_mode((450, 450))
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.blit(pygame.image.load(map_file), (0, 0))
 pygame.display.flip()
 while pygame.event.wait().type != pygame.QUIT:
-    pass
+    response = map_request(lon, lat, WIDTH, HEIGHT)
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    if event.type == pygame.KEYDOWN:
+        if event.key == pygame.K_PAGEUP:
+            if size != 17:
+                size += 1
+        elif event.key == pygame.K_PAGEDOWN:
+            if size != 0:
+                size -= 1
 pygame.quit()
 os.remove(map_file)
-
